b654130cb5c2eb4f6fed9923b4ae21c8b54a3663
Steven Nguyen <snguyen@oakland.edu>
2016-01-30 20:22:17 -0500

    Added code to play the beer song

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -4,11 +4,25 @@
 public class BeerSong {
 
     public static void main (String[] args) {
-        Ninety_Nine_Bottles_of_Beer();
+
 
     }
 
     private static void Ninety_Nine_Bottles_of_Beer(){
+        int count = 99;
+        while (count > 1) {
+            if (count == 2) { //changes bottles to bottle for better grammar of song
+                System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer");
+                count--;
+                System.out.println("take one down, pass it around, " + count + " bottle of beer on the wall.\n");
+            } else {
+                System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer");
+                count--;
+                System.out.println("take one down, pass it around, " + count + " bottles of beer on the wall.\n");
+            }
+        }
+        System.out.println(count + " bottle of beer on the wall, " + count + " bottle of beer\n" +
+        "take one down, pass it around, no more bottles of beer on the wall. :(");
 
 
     }
