74b3e1f778da54c6a9e9bdf2a8fa3329e32b7a6a
Steven Nguyen <snguyen@oakland.edu>
2016-01-30 19:49:40 -0500

    Added BeerSong.java file along with empty methods

---
+++ src/BeerSong.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Steven on 1/30/2016.
+ */
+public class BeerSong {
+
+    public static void main (String[] args) {
+        Ninety_Nine_Bottles_of_Beer();
+
+    }
+
+    private static void Ninety_Nine_Bottles_of_Beer(){
+
+
+    }
+}
